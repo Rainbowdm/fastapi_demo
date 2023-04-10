1ff42d0a7d2dc74e8ff38cf92cf561973a1d4627
Rainbowdm <dim8035@gmail.com>
2023-04-10 15:55:58 +0300

    03_007 passing fata to the api

--- ch03_first_api/main.py
+++ ch03_first_api/main.py
@@ -5,10 +5,13 @@ api = fastapi.FastAPI()
 
 
 @api.get('/api/calculate')
-def calculate():
-    value = 2 + 2
+def calculate(x: int, y: int, z: int = 10):
+    value = (x + y) * z
     return {
         'value': value
     }
+
+
+# Write in browser - http://127.0.0.1:8000/api/calculate?x=2&y=3&z=10
 
 uvicorn.run(api, port=8000, host="127.0.0.1")
