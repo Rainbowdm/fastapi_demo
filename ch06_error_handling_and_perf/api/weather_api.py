f50731082454b5c09dbb7d6dc83a42878d7c0171
Rainbowdm <dim8035@gmail.com>
2023-04-14 08:56:15 +0300

    06_003 error responses

--- ch06_error_handling_and_perf/api/weather_api.py
+++ ch06_error_handling_and_perf/api/weather_api.py
@@ -3,6 +3,7 @@ from typing import Optional
 import fastapi
 from fastapi import Depends
 from models.location import Location
+from models.validation_error import ValidationError
 from services import openweather_service
 
 router = fastapi.APIRouter()
@@ -10,6 +11,10 @@ router = fastapi.APIRouter()
 
 @router.get('/api/weather/{city}')
 async def weather(location: Location = Depends(), units: Optional[str] = 'metric'):
-    report = await openweather_service.get_report_async(location.city, location.state, location.country, units)
-    print(type(report), report)
-    return report
+    # report = await openweather_service.get_report_async(location.city, location.state, location.country, units)
+    # print(type(report), report)
+    # return report
+    try:
+        return await openweather_service.get_report_async(location.city, location.state, location.country, units)
+    except ValidationError as ve:
+        return fastapi.Response(content=ve.error_msg, status_code=ve.status_code)
